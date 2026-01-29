7b243b56bda06d885dea03c6247c5cec821d89a9
Mick <mihaingu@yahoo.com>
2026-01-29 13:41:44 +0200

    Update index-gOrejz-2.js

--- assets/index-gOrejz-2.js
+++ assets/index-gOrejz-2.js
@@ -9751,7 +9751,7 @@ function Kf() {
                             children: "RO"
                         })]
                     }), p.jsxs("a", {
-                        href: "https://demo.xatspaces.store/xatme/",
+                        href: "https://xatspaces.store",
                         target: "_blank",
                         rel: "noopener noreferrer",
                         className: "flex items-center gap-2 px-4 py-2 bg-white/10 rounded-xl border border-white/20 hover:bg-white/20 transition",
