024dd2a71daf1cdad157c71c0f66523f528d268b
Mick <mihaingu@yahoo.com>
2026-01-31 12:51:57 +0200

    Update index-gOrejz-2.js

--- assets/index-gOrejz-2.js
+++ assets/index-gOrejz-2.js
@@ -10134,7 +10134,7 @@ function qf() {
                     className: "flex-1 min-w-0",
                     children: p.jsx("div", {
                         className: "text-white font-semibold text-xs truncate",
-                        children: "Nicu Boieru - Caractere dalea fine..."
+                        children: "Dani M. - Voi nu ma haliti"
                     })
                 })]
             }), p.jsxs("div", {
